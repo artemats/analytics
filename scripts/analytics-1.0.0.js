ea8bcc1c29ae0f0accff2af365e8adb4d5522c08
Artem <artem.matseruk@gmail.com>
2024-10-28 17:12:17 +0200

    init commit

---
+++ scripts/analytics-1.0.0.js
@@ -0,0 +1,11 @@
+fetch('http://localhost:3000/api/hello', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({ name: 'John Doe' }),
+})
+	.then(response => response.json())
+	.then(data => console.log(data));
+
+console.log('hello')
